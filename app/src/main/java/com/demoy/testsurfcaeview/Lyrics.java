b4805f74422274998cd9df5ca0d21f3f824c3728
demony <yhc8245865@126.com>
2016-02-26 10:12:55 +0800

    testGit

--- app/src/main/java/com/demoy/testsurfcaeview/Lyrics.java
+++ app/src/main/java/com/demoy/testsurfcaeview/Lyrics.java
@@ -4,7 +4,7 @@ public class Lyrics {
 	public int begintime; // 开始时间
     public int endtime; // 结束时间  
     public int timeline; // 单句歌词用时  
-    public String lrc; // 单句歌词5
+    public String lrc; // 单句歌词
 
 
     public Lyrics(String lrc,int timeline) {
